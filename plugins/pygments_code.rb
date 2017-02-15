8e1bff7ed95dbe6c60184c2315e05033f911b972
PharaohKJ <kato@phalanxware.com>
2017-02-15 09:02:35 +0900

    fix pygments plugin

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -11,7 +11,10 @@ module HighlightCode
     lang = 'objc' if lang == 'm'
     lang = 'perl' if lang == 'pl'
     lang = 'yaml' if lang == 'yml'
-    str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
+    tmp = pygments(str, lang)
+    tmp = '' if tmp.nil?
+    str = tmp.match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '')
+    #strip out divs <div class="highlight">
     tableize_code(str, lang)
   end
 
